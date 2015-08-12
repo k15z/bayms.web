dda66ca33b975955fbfe38e58c792f8a2a2b3bd7
Kevin Zhang <kevin.2015.zhang@gmail.com>
2015-08-12 15:03:04 -0700

    Added "events" tab

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@ var baymsApp = angular.module('baymsApp', []);
 
 // Authentication & Tabs
 baymsApp.controller('baymsController', function($scope) {
+   $scope.user_id = sessionStorage.getItem('user_id');
    $scope.user_type = sessionStorage.getItem('user_type');
    $scope.user_name = sessionStorage.getItem('user_name');
    $scope.user_pass = sessionStorage.getItem('user_pass');
@@ -66,6 +67,202 @@ baymsApp.controller('profileController', function($scope) {
 
 // Events
 baymsApp.controller('eventsController', function($scope) {
+   $scope.isError = false;
+   $scope.isWorking = true;
+   function loadEvents() {
+      $scope.isError = false;
+      $scope.isWorking = true;
+      $.ajax({
+         method: "POST",
+         url: "./api/api.php?x=get_all_events",
+         dataType: "json",
+         data: {
+            "user_name": $scope.user_name,
+            "user_pass": $scope.user_pass
+         }
+      }).done(function(data) {
+         $scope.isWorking = false;
+         if (data) {
+            $scope.events = data;
+            $scope.isError = false;
+         } else {
+            $scope.isError = true;
+         }
+         $scope.$apply();
+         if ($scope.user_type >= 2)
+            $('.sortable').sortable();
+      }).error(function(err) {
+         $scope.isError = true;
+         $scope.isWorking = false;
+         $scope.$apply();
+      });
+   }; loadEvents();
+   $scope.createEvent = function() {
+      $scope.isError = false;
+      $scope.isWorking = true;
+      $.ajax({
+         method: "POST",
+         url: "./api/api.php?x=insert_event",
+         dataType: "json",
+         data: $.extend({
+            "user_name": $scope.user_name,
+            "user_pass": $scope.user_pass
+         }, $scope.create_event)
+      }).done(function(data) {
+         $scope.isWorking = false;
+         if (data) {
+            $scope.isError = false;
+         } else {
+            $scope.isError = true;
+         }
+         loadEvents();
+      }).error(function(err) {
+         $scope.isError = true;
+         $scope.isWorking = false;
+         loadEvents();
+      });
+   };
+   $scope.updateEvent = function(current_event) {
+      $scope.isError = false;
+      $scope.isWorking = true;
+      $.ajax({
+         method: "POST",
+         url: "./api/api.php?x=update_event",
+         dataType: "json",
+         data: $.extend({
+            "user_name": $scope.user_name,
+            "user_pass": $scope.user_pass
+         }, current_event)
+      }).done(function(data) {
+         $scope.isWorking = false;
+         if (data) {
+            $scope.isError = false;
+         } else {
+            $scope.isError = true;
+         }
+         loadEvents();
+      }).error(function(err) {
+         $scope.isError = true;
+         $scope.isWorking = false;
+         loadEvents();
+      });
+   };
+   $scope.deleteEvent = function(event_id) {
+      if (!confirm("Are you sure you want to delete this event?"))
+         return;
+      $scope.isError = false;
+      $scope.isWorking = true;
+      $.ajax({
+         method: "POST",
+         url: "./api/api.php?x=delete_event",
+         dataType: "json",
+         data: {
+            "user_name": $scope.user_name,
+            "user_pass": $scope.user_pass,
+            "event_id": event_id
+         }
+      }).done(function(data) {
+         $scope.isWorking = false;
+         if (data) {
+            $scope.isError = false;
+         } else {
+            $scope.isError = true;
+         }
+         loadEvents();
+      }).error(function(err) {
+         $scope.isError = true;
+         $scope.isWorking = false;
+         loadEvents();
+      });
+   };
+   $scope.saveOrder = function(event_id) {
+      var pieces = $('#sortable-'+event_id).find('tr');
+      for (var i = 0; i < pieces.length; i++) {
+         var data = {
+            piece_id: $(pieces[i]).attr('pid'),
+            piece_order: i
+         };
+         setTimeout(function(data) {
+            $scope.isError = false;
+            $scope.isWorking = true;
+            $.ajax({
+               method: "POST",
+               url: "./api/api.php?x=order_piece",
+               dataType: "json",
+               data: $.extend({
+                  "user_name": $scope.user_name,
+                  "user_pass": $scope.user_pass,
+               }, data)
+            }).done(function(data) {
+               $scope.isWorking = false;
+               if (data) {
+                  $scope.isError = false;
+               } else {
+                  $scope.isError = true;
+               }
+            }).error(function(err) {
+               $scope.isError = true;
+               $scope.isWorking = false;
+            });
+         }, i*100, data)
+      };
+      setTimeout(loadEvents, pieces.length*100);
+   };
+   $scope.approvePiece = function(piece_id, approved) {
+      $scope.saveOrder(piece_id);
+      $scope.isError = false;
+      $scope.isWorking = true;
+      $.ajax({
+         method: "POST",
+         url: "./api/api.php?x=approve_piece",
+         dataType: "json",
+         data: {
+            "user_name": $scope.user_name,
+            "user_pass": $scope.user_pass,
+            "piece_id": piece_id,
+            "approved": approved
+         }
+      }).done(function(data) {
+         $scope.isWorking = false;
+         if (data) {
+            $scope.isError = false;
+         } else {
+            $scope.isError = true;
+         }
+         loadEvents();
+      }).error(function(err) {
+         $scope.isError = true;
+         $scope.isWorking = false;
+         loadEvents();
+      });
+   }
+   $scope.submitPiece = function(event_id) {
+      $scope.isError = false;
+      $scope.isWorking = true;
+      $.ajax({
+         method: "POST",
+         url: "./api/api.php?x=submit_piece",
+         dataType: "json",
+         data: $.extend({
+            "user_name": $scope.user_name,
+            "user_pass": $scope.user_pass,
+            "event_id": event_id
+         }, $scope.submit_piece)
+      }).done(function(data) {
+         $scope.isWorking = false;
+         if (data) {
+            $scope.isError = false;
+            $scope.submit_piece = {};
+         } else {
+            $scope.isError = true;
+         }
+         loadEvents();
+      }).error(function(err) {
+         $scope.isError = true;
+         $scope.isWorking = false;
+         loadEvents();
+      });
+   }
 });
 
 // Members
