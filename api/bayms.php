fc5714735b8b35bb92c35677f0e1d20fb6426d0d
Kevin Zhang <kevin.2015.zhang@gmail.com>
2015-08-14 10:44:27 -0700

    Added student_birthday & parent_2_phone

--- api/bayms.php
+++ api/bayms.php
@@ -158,8 +158,8 @@ class BAYMS {
       $user_id = $this->verifyUserID($user_id);
 
       $relevant = [
-         "student_name", "student_phone", "student_email",
-         "parent_name", "parent_phone", "parent_email",
+         "student_name", "student_phone", "student_email", "student_birthday",
+         "parent_name", "parent_phone", "parent_email", "parent_2_phone",
          "home_address", "current_school",
          "instrument_1", "instrument_1_about",
          "instrument_2", "instrument_2_about",
