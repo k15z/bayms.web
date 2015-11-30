8e0df4fcea05c608a09911f10bfc0b554fd3b025
unbrace3 <unbrace3@gmail.com>
2015-11-29 20:52:18 -0800

    CalendarView
    Basic Android app that displays the program based on the event IDs.
    Swipe to go to next event

---
+++ CalendarView/app/src/main/java/bayms/scheduleview/MainActivity.java
@@ -0,0 +1,95 @@
+package bayms.scheduleview;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.*;
+import android.view.*;
+import android.widget.*;
+import android.app.*;
+
+public class MainActivity extends Activity {
+
+    String url = "https://dev.bayms.org/program.htm#15";
+    private float x1,x2;
+    static final int MIN_DISTANCE = 150;
+    Integer schedID=15;
+    WebView myWebView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        displayURL();
+    }
+
+    public void displayURL()
+    {
+        myWebView.clearCache(true);
+        myWebView.clearView();
+        myWebView.reload();
+        url="https://dev.bayms.org/program.htm#"+schedID.toString();
+        myWebView.loadUrl(url);
+        myWebView.loadUrl(url);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public boolean onTouchEvent(MotionEvent event)
+    {
+        switch(event.getAction())
+        {
+            case MotionEvent.ACTION_DOWN:
+                x1 = event.getX();
+                break;
+            case MotionEvent.ACTION_UP:
+                x2 = event.getX();
+                float deltaX = x2 - x1;
+                if (Math.abs(deltaX) > MIN_DISTANCE)
+                {
+                    if (x2 > x1)
+                    {
+                        schedID--;
+                        Toast.makeText(this, "Left "+schedID.toString()+" to Right swipe [Next]", Toast.LENGTH_SHORT).show ();
+                    }
+
+                    // Right to left swipe action
+                    else
+                    {
+                        Toast.makeText(this, "Right to Left swipe [Previous]", Toast.LENGTH_SHORT).show ();
+                        schedID++;
+                    }
+                    displayURL();
+                }
+                else
+                {
+                    // consider as something else - a screen tap for example
+                }
+                break;
+        }
+        return super.onTouchEvent(event);
+    }
+}
