6ed9a176163262508a8aaae114ac9e3411336c32
zeNightq <danielliuispro@gmail.com>
2016-01-09 13:06:13 -0800

    Revert "Added 2016 calendar download"
    This reverts commit b6de4eea7c05b5c99981e553cd46ed3fdc2f40ce.

--- js/app.js
+++ js/app.js
@@ -671,8 +671,6 @@ baymsApp.controller('newsController', ['$scope', '$sce', function($scope, $sce) 
       $scope.isWorking = true;
       $.ajax({
          method: "POST",
-         
-         
          url: "./api/api.php?x=delete_news",
          dataType: "json",
          data: $.extend({}, $scope.auth, {
@@ -699,8 +697,3 @@ baymsApp.controller('newsController', ['$scope', '$sce', function($scope, $sce) 
       return $sce.trustAsHtml(converter.makeHtml(text));
    }
 }]);
-
-baymsApp.controller('calendarController', function($scope) {
-	   $scope.isError = false;
-	   $scope.isWorking = true;
-});
